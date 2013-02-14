68abd714f4a6f66056a8719daa7e6878e41f40b5
Patrick <patrick@X1.(none)>
2013-02-13 16:43:27 -0800

    test commit

---
+++ Sum3.java
@@ -0,0 +1,45 @@
+package leetcode;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array S of n integers, 
+ * are there elements a, b, c in S such that a + b + c = 0? 
+ * Find all unique triplets in the array which gives the sum of zero.
+ * There could be duplicated elements in the array.
+ * 
+ * @author patrick
+ * @date   2012.7.11
+ */
+public class Sum3 {
+	
+	public static void main(String[] args){
+			int[][]	a = new int[5][7];
+			String s = "213213";
+			Map<String,Integer> map = new HashMap<String,Integer>();
+			System.out.println(a.length+"--"+a[0].length);
+	}
+	
+   
+
+}
+
+class WithStatic{
+//	static void superStatic(){
+//		System.out.println("Super");
+//	}
+	
+	void superStatic(){
+		
+	}
+}
+
+//class Sub{
+//	@Override
+//	void superStatic(){
+//		System.out.println("Super");
+//	}
+//}
+
+
+
