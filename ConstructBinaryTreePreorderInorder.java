730c20daa73d1dd46d1475e744ef17c91b0d77d6
Yuan <patrickyao1988@gmail.com>
2013-02-23 15:44:40 -0800

    ConstructBinaryTreePreorderInorder

--- ConstructBinaryTreePreorderInorder.java
+++ ConstructBinaryTreePreorderInorder.java
@@ -5,7 +5,8 @@ package leetcode;
  * @date 2012.10.3 PASS
  *
  */
-public class ConstructBinaryTreePreorderInorder {	
+public class ConstructBinaryTreePreorderInorder {
+	
 	    public TreeNode buildTree(int[] inorder, int[] preorder) {
 	    	//empty input check
 	    	if(inorder==null||inorder.length==0){
@@ -31,17 +32,6 @@ public class ConstructBinaryTreePreorderInorder {
 	    	int data = pre[pre_start];
 	    	TreeNode root = new TreeNode(data);
 	    	
-//          codes below may reduce some function calls, but not necessary.
-//          since if in_start == in_end -1, 
-//          then right :	   in_index+1 [start]  will = in_end [end]
-//          and  left  :       in_start   [start]  will = in_index [end]
-//          which will both equal null by function call again. and results exactly same as below codes.
-	    	
-//	    	if(in_start==in_end-1){
-//	    		root.left=null;
-//	    		root.right=null;
-//	    		return root;
-//	    	}
 	    	int in_index=in_start;
 	    	
 	    	for(int i=in_start;i<in_end;i++){
@@ -53,13 +43,7 @@ public class ConstructBinaryTreePreorderInorder {
 	    	root.left = recursiveBuild(in,pre,in_start,in_index,pre_start+1);
 	    	root.right = recursiveBuild(in,pre,in_index+1,in_end,in_index-in_start+pre_start+1);
 	    	return root;
-	    }
-	    
-	    public static void main(String[] args){
-	    	int[] pre = {1,2};
-	    	int[] in  = {2,1};
-	        new ConstructBinaryTreePreorderInorder().buildTree(pre,in);
-	    }
+	   }
 }
 
 
