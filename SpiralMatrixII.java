e03defee8f51fe4221aca7749aa9e7b06efd1f40
Yuan <patrickyao1988@gmail.com>
2013-02-22 21:30:35 -0800

    Spiral Matrix II

---
+++ SpiralMatrixII.java
@@ -0,0 +1,40 @@
+/**
+Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+You should return the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+**/
+
+public class SprialMatrixII {
+    public int[][] generateMatrix(int n) {
+       int[][] matrix = new int[n][n];
+       if(n<1)
+            return matrix;
+       int val=1;
+       for(int i=0;i<=n/2;i++){
+            //top elements
+            for(int j=i;j<matrix[0].length-i-1;j++)
+                matrix[i][j]=val++;
+            //right elements
+            for(int j=i;j<matrix.length-i-1;j++)
+                matrix[j][matrix[0].length-1-i]=val++;
+            //bottom elements
+            for(int j=matrix[0].length-1-i;j>i;j--)
+                matrix[matrix.length-1-i][j]=val++;
+            //left elements
+            for(int j=matrix.length-i-1;j>i;j--)
+                matrix[j][i]=val++;
+        }
+        if((n&1)==1)
+            matrix[n/2][n/2]=val;
+        return matrix;
+        
+    }
+}
