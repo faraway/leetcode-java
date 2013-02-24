5b26f12b31a02920a86c3fbf1fc0ba4bd2439c47
Yuan <patrickyao1988@gmail.com>
2013-02-23 21:48:21 -0800

    MaximumSubarray

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,12 +1,12 @@
 package leetcode;
 
 public class MaximumSubarray {
-	/**
-	 * Regular Algorithm
-	 * @param A
-	 * @return
-	 */
-	public int maxSubArray(int[] A) {
+    /**
+     * Regular Algorithm
+     * @param A
+     * @return
+    */
+    public int maxSubArray(int[] A) {
         int max = Integer.MIN_VALUE;
         int sum = 0;
         for(int i=0;i<A.length;i++ ){
