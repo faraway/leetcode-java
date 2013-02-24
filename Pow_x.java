40681ea0a41d2e1d35ca042e424662819fbd1e8f
Yuan <patrickyao1988@gmail.com>
2013-02-23 23:21:22 -0800

    Pow(x)

--- Pow_x.java
+++ Pow_x.java
@@ -1,16 +1,30 @@
 package leetcode;
 
-public class Pow_x {
-	//brute force solution
-	public class Solution {
-	    public double pow(double x, int n) {
-	        if(n==0)
-	            return 1;
-	        double result = x;
-	        for(int i=0;i<Math.abs(n)-1;i++){
-	            result*=x;
-	        }
-	        return n<0? 1.0/result:result;
-	    }
-	}
+public class PowX {
+
+    public double pow(double x, int n) {
+        if(n==0 )
+            return 1.0 ;
+        if(x==1.0 )
+            return x;
+        if(x==-1.0 )
+            return n%2 ==0 ? 1.0:-1.0;
+           
+        int sign = n>0 ? 1 :-1 ;
+        n=sign> 0 ? n:-n;
+       
+        double pow = getPow(x,n);
+        return sign > 0 ? pow : 1.0/pow;
+
+    }
+   
+    private double getPow(double x, int n){
+        if(n==1 )
+            return x;
+        int odd = n%2 ;
+        int half = n/2 ;
+        double temp = getPow(x,half);
+        return odd==1 ? temp*temp*x : temp*temp;
+    }
 }
+
