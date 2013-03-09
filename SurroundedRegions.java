877aa3fa87786560e7ee14b8048aeeaf57ac3fca
Yuan <patrickyao1988@gmail.com>
2013-03-08 22:16:20 -0800

    Update SurroundedRegions.java

--- SurroundedRegions.java
+++ SurroundedRegions.java
@@ -63,3 +63,85 @@ public class SurroundedRegions {
         captureArea(i,j+1,board);
     }
 }
+
+/**
+ * BFS solution
+ * But it is slow than DFS one, more improvements can be made
+ * */
+ class SurroundedRegionsBFS {
+    
+    Queue<Point> q;
+    
+    public void solve(char[][] board) {
+        int m=board.length;
+        if(m==0)
+            return;
+        int n=board[0].length;
+        
+        q = new LinkedList<Point>();
+        //top
+        for(int i=0;i<n;i++){
+            if(board[0][i]=='O')
+                bfs(0,i,board);
+        }
+        //right
+        for(int i=0;i<m;i++){
+            if(board[i][n-1]=='O')
+                bfs(i,n-1,board);
+        }
+        //bot
+        for(int i=0;i<n;i++){
+            if(board[m-1][i]=='O')
+                bfs(m-1,i,board);
+        }
+        //left
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O')
+                bfs(i,0,board);
+        }
+        //final traveral of the board
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                //unsurrounded Os
+                if(board[i][j]=='.')
+                    board[i][j]='O';
+                //surrounded Os
+                else if(board[i][j]=='O')
+                    board[i][j]='X';
+            }
+        }
+        
+    }
+    
+    private void bfs(int i,int j, char[][] board){
+        q.offer(new Point(i,j));
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            board[p.i][p.j]='.';
+            if(valid(p.i+1,p.j,board))
+                q.offer(new Point(p.i+1,p.j));
+            if(valid(p.i-1,p.j,board))
+                q.offer(new Point(p.i-1,p.j));
+            if(valid(p.i,p.j+1,board))
+                q.offer(new Point(p.i,p.j+1));
+            if(valid(p.i,p.j-1,board))
+                q.offer(new Point(p.i,p.j-1));
+        }
+    }
+    
+    private boolean valid(int i, int j, char[][] board){
+        if(i<0||i>=board.length||j<0||j>=board[0].length)
+            return false;
+        if(board[i][j]=='O')
+            return true;
+        return false;
+    }
+}
+
+class Point{
+    int i,j;
+    Point(int i,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
