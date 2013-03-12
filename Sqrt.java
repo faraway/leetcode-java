747ef9adf09e5d01a98903932d181e733beb682e
Yuan <patrickyao1988@gmail.com>
2013-03-11 18:14:57 -0700

    SquareRoot

--- Sqrt.java
+++ Sqrt.java
@@ -1,19 +1,30 @@
-
 /**
  *Newton iteration. <= 20 iterate times will get the answer.
  **/
 public class Sqrt {
-      
-       public static double sqrt(double x){
+       
+       //accuracy providede version
+       public static double sqrt(double x,double acuracy){
              double sqrt = x;
-             while(Math .abs(sqrt*sqrt-x)> 0.01){
+             while(Math .abs(sqrt*sqrt-x)> acuracy){
                   sqrt=(sqrt+x/sqrt)/ 2;
              }
              return sqrt;
-      }
+       }
+      
+       //without accuracy version. Still only several times of iteration
+       public static double sqrt(double x){
+      	double sqrt = x;
+		double pre = 0;
+		while(sqrt!=pre){
+			pre=sqrt;
+			sqrt=(sqrt+x/sqrt)/2;
+		}
+		return sqrt;
+	 }
       
        public static void main(String[] args){
              System.out .println(sqrt( 1231311244.0));
-      }
+       }
 
 }
